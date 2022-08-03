2720454b7bc00dc322aab4ac48cfb97aeedc7898
Luis Iturrios <luisiturrios1@gmail.com>
2022-08-02 21:32:56 -0700

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Luis Iturrios on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
